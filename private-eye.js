6353a71b4acfc9f1be5979277865a8ec4e4631dd
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-01-21 20:01:38 -0500

    Add custom warning message functionality;
    This patch adds the custom warning functionality in two different ways:
    
    1. Passing a configuration object to match the URL and attach a custom message.
    2. Leaving the title attribute untouched if there is already one.

--- private-eye.js
+++ private-eye.js
@@ -6,11 +6,36 @@
     styles.innerHTML = 'a.private-link::after { content: "\\1F512"; font-size: 0.75em; vertical-align: top; }';
     document.body.appendChild(styles);
 
+    var DEFAULT_MESSAGE = "This is a link to a private site, which may or may not be accessible to you.";
+    var hrefValue;
+    var linkMessage;
+
     opts.ignoreUrls.forEach(function(url) {
-      var anchors = document.querySelectorAll('a[href*="' + url + '"]');
+
+      hrefValue = url;
+      linkMessage = DEFAULT_MESSAGE;
+
+      // If the `url` is an Object, then parse the properties `message` & `url`
+      //
+      if ( url === Object( url ) ) {
+
+        linkMessage = url.message;
+        hrefValue = url.url;
+
+      }
+
+      var anchors = document.querySelectorAll('a[href*="' + hrefValue + '"]');
       Array.prototype.forEach.call(anchors, function(anchor) {
         anchor.className += ' private-link';
-        anchor.title = "This is a link to a private site, which may or may not be accessible to you.";
+
+        // Only replace the anchor's title if it is empty
+        //
+        if ( '' === anchor.title ) {
+
+          anchor.title = linkMessage;
+
+        }
+
       });
     });
   };
